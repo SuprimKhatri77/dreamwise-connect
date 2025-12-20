c5da497216816ee75f7f8a03a5aac56b3a52e8bb
Bsal <bishalm626@gmail.com>
2025-12-20 11:15:09 +0545

    change num

--- app/australia-visa/page.tsx
+++ app/australia-visa/page.tsx
@@ -1,10 +1,8 @@
 import ApplyAustraliaVisaFromDubai from "@/modules/Australia/ApplyAustralia";
 
-export default function page() {
-  const whatsappNumber = process.env.WHATSAPP_NUMBER!;
-  return (
-    <>
-      <ApplyAustraliaVisaFromDubai whatsappNumber={whatsappNumber} />
-    </>
-  );
+export default function Page() {
+  const whatsappNumber =
+    process.env.WHATSAPP_NUMBER ?? "";
+
+  return <ApplyAustraliaVisaFromDubai whatsappNumber={whatsappNumber} />;
 }
