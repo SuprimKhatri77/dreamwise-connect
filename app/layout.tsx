bf24c4b8bafa9061d69f4f51f58faa4afa55c1da
Suprim Khatri <Supremekhatri62@gmail.com>
2026-01-02 18:38:49 +0545

    added tag manager script

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,18 +44,29 @@ export default function RootLayout({
         {/* Google Analytics */}
         {process.env.NODE_ENV === "production" && (
           <>
-            <Script
-              src="https://www.googletagmanager.com/gtag/js?id=G-85X2J3CSS1"
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="gtm" strategy="afterInteractive">
               {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-85X2J3CSS1');
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KS55WQHH');
           `}
             </Script>
+
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-KS55WQHH"
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
           </>
         )}
         <TopHeader whatsappNumber={whatsappNumber} />
